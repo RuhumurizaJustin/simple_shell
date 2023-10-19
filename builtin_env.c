b49cddb5ecf7fb9386551228150db68d0f8da79d
Ofallbrains <ofallbrains@gmail.com>
2023-10-19 02:59:41 -0700

    shell project

---
+++ builtin_env.c
@@ -0,0 +1,91 @@
+#include "simple_shell.h"
+
+/**
+ * builtin_env - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_env(program_data *data)
+{
+	int j;
+	char cpname[50] = {'\0'};
+	char *var_copy = NULL;
+
+	/* if not arguments */
+	if (data->tokens[1] == NULL)
+		print_environ(data);
+	else
+	{
+		for (j = 0; data->tokens[1][j]; j++)
+		{
+			if (data->tokens[1][j] == '=')
+			{
+				var_copy = str_duplicate(env_get_key(cpname, data));
+				if (var_copy != NULL)
+					env_set_key(cpname, data->tokens[1] + j + 1, data);
+
+				print_environ(data);
+				if (env_get_key(cpname, data) == NULL)
+				{
+					_print(data->tokens[1]);
+					_print("\n");
+				}
+				else
+				{
+					env_set_key(cpname, var_copy, data);
+					free(var_copy);
+				}
+				return (0);
+			}
+			cpname[j] = data->tokens[1][j];
+		}
+		errno = 2;
+		perror(data->command_name);
+		errno = 127;
+	}
+	return (0);
+}
+
+/**
+ * builtin_set_env - ..
+ * @data: struct for the program's data
+ * Return: zero if success
+ * or other number if its declared in the arguments
+ */
+int builtin_set_env(program_data *data)
+{
+	/* validate args */
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
+	if (data->tokens[3] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+
+	env_set_key(data->tokens[1], data->tokens[2], data);
+
+	return (0);
+}
+
+/**
+ * builtin_unset_env - ..
+ * @data: struct for the program's data'
+ * Return: o
+ */
+int builtin_unset_env(program_data *data)
+{
+	/* validate args */
+	if (data->tokens[1] == NULL)
+		return (0);
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	env_remove_key(data->tokens[1], data);
+
+	return (0);
+}
